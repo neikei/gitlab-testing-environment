fd98935f6ead6724f99b0b61708aa5484e8c2546
neikei <niklas@vosskoetter.org>
2018-01-31 19:14:37 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# gitlab-testing-environment
+
+Gitlab testing environment in a Vagrantbox based on Debian and provisioned with Ansible.
+
+## Requirements
+
+- Virtualbox >= 5.2.4
+- Vagrant >= 2.0.1
+- Vagrant Plugins:
+  - vagrant plugin install vagrant-hostmanager
+  - vagrant plugin install vagrant-vbguest
+
+## Getting started
+
+1. git clone https://github.com/neikei/gitlab-testing-environment.git
+2. cd gitlab-testing-environment
+3. vagrant up
+4. ... wait ...
+5. Open Gitlab in your web browser: http://gitlab.test
+
+## Feedback, Issues and Pull-Requests
+
+Feel free to report issues, fork this project and submit pull requests.
---
+++ ansible/tools/install_ansible_in_Vagrantbox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+debian_version=`cat /etc/debian_version`
+ansible_installed=`which ansible | wc -l`
+
+if [ $ansible_installed -eq 0 ]; then
+
+  # Install dirmngr if Debian major version is 9
+  if [[ $debian_version =~ ^9.*$ ]]; then
+    sudo apt install -y dirmngr
+  fi
+
+  # Add ansible repository, install ansible 2.4.2.0 and set it on hold
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main" >> /etc/apt/sources.list.d/ansible.list
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  sudo apt update -y
+  sudo apt install -y ansible=2.4.2.0-1ppa~xenial
+  sudo apt-mark hold ansible
+
+else
+  echo "Ansible is already installed."
+fi
+
+echo "==> Ansible version <=="
+ansible --version
+echo "==> Ansible version <=="
